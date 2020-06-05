8ae7dc4f8497eeeee5b2b81299a0494abfe64faa
Fabio Italiano <fabio.italiano@outlook.com>
2020-06-05 18:16:44 -0400

    protocol and delegate

--- MatchApp/Controller/ViewController.swift
+++ MatchApp/Controller/ViewController.swift
@@ -8,16 +8,49 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    @IBOutlet weak var collectionView: UICollectionView!
+    
     
     let model = CardModel()
     var cardsArray = [Card]()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        cardsArray = model.getCards()
+        
+        // set the view controller as the datasource and delegate of the collection view
+        
+        collectionView.dataSource   = self
+        collectionView.delegate = self
+    
+    
     }
 
+    
+    // MARK:
+    
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        
+        // return the number of cards
+        return cardsArray.count
+        
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        
+        // get a cell
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "CardCell", for: indexPath)
+        
+        // TODO: configure it
+        
+        
+        // return it
+        return cell
+        
+    }
 
 }
 
